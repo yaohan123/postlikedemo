a96981c3b4832cdf16d76037a782a1684cf5fb68
yaohan <250534908@qq.com>
2020-08-23 17:18:23 +0800

    点赞v2

---
+++ src/main/java/com/yaohan/postlike/PostlikeApplication.java
@@ -0,0 +1,13 @@
+package com.yaohan.postlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostlikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostlikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaohan/postlike/PostlikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaohan.postlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostlikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
